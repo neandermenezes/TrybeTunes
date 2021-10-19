0775a669c8e99c54dd7f826d654a886db92dd524
neandermenezes <neander2801@gmail.com>
2021-10-19 15:30:34 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Route } from 'react-router';
+import Login from './components/Login';
+import Search from './components/Search';
+import Album from './components/Album';
+import Favorites from './components/Favorites';
+import Profile from './components/Profile';
+import ProfileEdit from './components/ProfileEdit';
+import NotFound from './components/NotFound';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <p>TRYBE TUNES!</p>
-        <p>a</p>
-      </div>
+      <BrowserRouter>
+        <Route exact path="/" render={ (props) => <Login { ...props } /> } />
+        <Route path="/search" render={ (props) => <Search { ...props } /> } />
+        <Route path="/album/:id" render={ (props) => <Album { ...props } /> } />
+        <Route path="/favorites" render={ (props) => <Favorites { ...props } /> } />
+        <Route path="/profile" render={ (props) => <Profile { ...props } /> } />
+        <Route
+          path="/profile/edit"
+          render={ (props) => <ProfileEdit { ...props } /> }
+        />
+        <Route path="/" render={ (props) => <NotFound { ...props } /> } />
+      </BrowserRouter>
     );
   }
 }
---
+++ src/components/Search.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Search extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-search">
+        <p>placeholder</p>
+      </div>
+    );
+  }
+}
+
+export default Search;
